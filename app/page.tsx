8d7486d252abb7657c32c4cdac5a2c7a7c0f8e50
Angel Castañeda <luisacast.29@gmail.com>
2025-10-28 21:26:16 -0600

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,21 @@
-import Image from "next/image";
+import { HERO } from "@/data/constante";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            {HERO.TITLE}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            {HERO.SUBTITLE}{" "}
           </p>
+          <img
+            src="https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464621534_1312533213530471_8445062473960682421_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xUcyeBbwTUcQ7kNvwEvn6YZ&_nc_oc=AdlsI_uTrmtkwa6KHT0CgdcHZv6AxQxMG2-vqLflcgMdocrFSwHa83xlPXpctQKRVr0&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=4P7asWVVcDCCMdy-MQ6mzA&oh=00_Afc6OgavYmS56ny3oZvDWSjTzRXn3wlbD_yWHX9I1w08jg&oe=69077088"
+            alt=""
+            className="rounded-full w-44 h-44 object-cover"
+          />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
@@ -41,22 +24,15 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            Proyectos
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid px-5 transition-colors hover:border-transparent dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Github
           </a>
         </div>
       </main>
---
+++ data/constante.ts
@@ -0,0 +1,18 @@
+export const HERO = {
+  TITLE: "Hola, soy Ian",
+  SUBTITLE:
+    "Desarrollador apasionado del mundo de los datos, encontrar las mejores formas de innovar y compartir conocimiento",
+  PROYECTOS: { text: "ve mis proyectos", href: "https://github.com/angelcstd" },
+};
+/**
+ * inicio
+ * sobre mi
+ * proyectos
+ * menciones
+ * contacto
+ */
+
+/**
+ * bento grid
+ *
+ */
