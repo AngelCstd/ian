daf9abd28b55a8ba9ebb09fcc3054d0b0c4fc0f0
Angel Castañeda <luisacast.29@gmail.com>
2025-10-28 21:41:44 -0600

    agregando objeto

--- data/constante.ts
+++ data/constante.ts
@@ -3,7 +3,25 @@ export const HERO = {
   SUBTITLE:
     "Desarrollador apasionado del mundo de los datos, encontrar las mejores formas de innovar y compartir conocimiento",
   PROYECTOS: { text: "ve mis proyectos", href: "https://github.com/angelcstd" },
+  IMAGE: {
+    href: "https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464621534_1312533213530471_8445062473960682421_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xUcyeBbwTUcQ7kNvwEvn6YZ&_nc_oc=AdlsI_uTrmtkwa6KHT0CgdcHZv6AxQxMG2-vqLflcgMdocrFSwHa83xlPXpctQKRVr0&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=4P7asWVVcDCCMdy-MQ6mzA&oh=00_Afc6OgavYmS56ny3oZvDWSjTzRXn3wlbD_yWHX9I1w08jg&oe=69077088",
+    alt: "Foto de perfil de Ian",
+  },
 };
+
+export const PROYECTOS = [
+  {
+    nombre: "",
+    github: "",
+    youtube: {
+      src: "",
+      title: "",
+    },
+    url: "",
+    linkedin: "",
+    imagen: "",
+  },
+];
 /**
  * inicio
  * sobre mi
